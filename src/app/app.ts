b3577aa9227596374ad1ff75e9733eec1fe7cf17
Antony Giomar <49871651+antonygiomarx@users.noreply.github.com>
2020-01-08 14:48:06 -0600

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -59,40 +59,94 @@
 //   console.log(name, JSON.stringify(event, null, 2));
 // }
 
-async function main() {
+// Imports the Google Cloud client library
+// const fs = require('fs');
+// const speech = require('@google-cloud/speech');
+
+// // Creates a client
+// const client = new speech.SpeechClient();
+
+// /**
+//  * TODO(developer): Uncomment the following lines before running the sample.
+//  */
+// const filename = './assets/test.ogg';
+// const encoding = 'LINEAR16';
+// const sampleRateHertz = 16000;
+// const languageCode = 'es-ES';
+
+// const config = {
+//   encoding: encoding,
+//   sampleRateHertz: sampleRateHertz,
+//   languageCode: languageCode
+// };
+// const audio = {
+//   content: fs.readFileSync(filename).toString('base64')
+// };
+
+// const request = {
+//   config: config,
+//   audio: audio
+// };
+
+// // Detects speech in the audio file
+
+// const [response] = await client.recognize(request);
+// const transcription = response.results
+//   .map((result: any) => result.alternatives[0].transcript)
+//   .join('\n');
+// console.log(`Transcription: `, transcription);
+//$env:GOOGLE_APPLICATION_CREDENTIALS="C:\Users\antony.hernandez\Documents\GitHub\Speech-to-Text\379007511876-fj9dbhuem3lct4bm9gkfa5kh920am4vl.apps.googleusercontent.com_secreto_cliente.json"
+
+async function syncRecognize() {
+  // [START speech_transcribe_sync]
   // Imports the Google Cloud client library
-  const speech = require('@google-cloud/speech');
   const fs = require('fs');
+  const speech = require('@google-cloud/speech');
 
   // Creates a client
   const client = new speech.SpeechClient();
 
-  // The name of the audio file to transcribe
-  const fileName = './assets/test.ogg';
+  /**
+   * TODO(developer): Uncomment the following lines before running the sample.
+   */
+  const filename = './assets/test3.ogg';
+  const encoding = 'OGG_OPUS';
+  const sampleRateHertz = 16000;
+  const languageCode = 'es-ES';
+  const WordConfidence = true;
 
-  // Reads a local audio file and converts it to base64
-  const file = fs.readFileSync(fileName);
-  const audioBytes = file.toString('base64');
-
-  // The audio file's encoding, sample rate in hertz, and BCP-47 language code
-  const audio = {
-    content: audioBytes
-  };
   const config = {
-    encoding: 'LINEAR16',
-    sampleRateHertz: 16000,
-    languageCode: 'en-US'
+    encoding: encoding,
+    sampleRateHertz: sampleRateHertz,
+    languageCode: languageCode,
+    enableWordConfidence: WordConfidence
   };
+  const audio = {
+    content: fs.readFileSync(filename).toString('base64')
+  };
+
   const request = {
-    audio: audio,
-    config: config
+    config: config,
+    audio: audio
   };
 
   // Detects speech in the audio file
   const [response] = await client.recognize(request);
+
+  /* Transcription*/
   const transcription = response.results
     .map((result: any) => result.alternatives[0].transcript)
     .join('\n');
-  console.log(`Transcription: ${transcription}`);
+  const confidence = response.results
+    .map((result: any) => result.alternatives[0].confidence)
+    .join(`\n`);
+  console.log(`Transcription: ${transcription} \nConfidence: ${confidence}`);
+
+  console.log(`Word-Level-Confidence:`);
+  const words = response.results.map((result: any) => result.alternatives[0]);
+  words[0].words.forEach((a: any) => {
+    console.log(` word: ${a.word}, confidence: ${a.confidence}`);
+  });
+  // [END speech_transcribe_sync]
 }
-main().catch(console.error);
+syncRecognize();
